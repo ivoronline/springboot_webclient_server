e643001d7f1dacf2c8c87430d89e5f210c512bd6
igoreta <ivoronline@gmail.com>
2021-02-23 11:36:53 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_webclient_server/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_webclient_server/controllers/MyController.java
@@ -3,10 +3,18 @@ package com.ivoronline.springboot_webclient_server.controllers;
 import com.ivoronline.springboot_webclient_server.entities.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MyController {
 
+  //===============================================================
+  // GET PERSON
+  //===============================================================
+  @ResponseBody
   @RequestMapping("/GetPerson")
   public Person getPerson() {
 
@@ -21,4 +29,35 @@ public class MyController {
 
   }
 
+  //===============================================================
+  // GET PERSONS
+  //===============================================================
+  @ResponseBody
+  @RequestMapping("/GetPersons")
+  public List<Person> getPersons() {
+
+    //CREATE PERSON 1
+    Person person1      = new Person();
+           person1.id   = 1;
+           person1.name = "John";
+           person1.age  = 20;
+
+    //CREATE PERSON 2
+    Person person2      = new Person();
+           person2.id   = 2;
+           person2.name = "Bill";
+           person2.age  = 50;
+
+    //CREATE LIST
+    List<Person> persons = new ArrayList();
+                 persons.add(person1);
+                 persons.add(person2);
+
+    //RETURN PERSON
+    return persons;
+
+  }
+
 }
+
+

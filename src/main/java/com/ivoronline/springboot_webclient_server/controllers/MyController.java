547b5afb305ab09bfadc78acecc06d6c3c3d6afc
igoreta <ivoronline@gmail.com>
2022-03-30 12:45:52 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_webclient_server/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_webclient_server/controllers/MyController.java
@@ -1,59 +1,47 @@
 package com.ivoronline.springboot_webclient_server.controllers;
 
 import com.ivoronline.springboot_webclient_server.entities.Person;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class MyController {
+
+  //===============================================================
+  // GET TEXT
+  //===============================================================
+  @RequestMapping("GetText")
+  String getText(@RequestParam String name) {
+    return "Hello " + name;
+  }
 
   //===============================================================
   // GET PERSON
   //===============================================================
-  @ResponseBody
-  @RequestMapping("/GetPerson")
-  public Person getPerson() {
-
-    //CREATE PERSON
-    Person person      = new Person();
-           person.id   = 1;
-           person.name = "Susan";
-           person.age  = 30;
-
-    //RETURN PERSON
-    return person;
-
+  @RequestMapping("GetPerson")
+  Person getPerson() {
+    return new Person(1, "Susan", 30);
   }
 
   //===============================================================
   // GET PERSONS
   //===============================================================
-  @ResponseBody
-  @RequestMapping("/GetPersons")
-  public List<Person> getPersons() {
+  @RequestMapping("GetPersons")
+  List<Person> getPersons() {
 
-    //CREATE PERSON 1
-    Person person1      = new Person();
-           person1.id   = 2;
-           person1.name = "John";
-           person1.age  = 40;
-
-    //CREATE PERSON 2
-    Person person2      = new Person();
-           person2.id   = 3;
-           person2.name = "Bill";
-           person2.age  = 50;
+    //CREATE PERSONS
+    Person person1 = new Person(2, "John", 40);
+    Person person2 = new Person(3, "Bill", 50);
 
     //CREATE LIST
     List<Person> persons = new ArrayList();
                  persons.add(person1);
                  persons.add(person2);
 
-    //RETURN PERSON
+    //RETURN PERSONS
     return persons;
 
   }
--- src/main/java/com/ivoronline/springboot_webclient_server/entities/Person.java
+++ src/main/java/com/ivoronline/springboot_webclient_server/entities/Person.java
@@ -1,7 +1,17 @@
 package com.ivoronline.springboot_webclient_server.entities;
 
 public class Person {
+
+  //PROPERTIES
   public Integer id;
   public String  name;
   public Integer age;
+
+  //CONSTRUCTOR
+  public Person(Integer id, String name, Integer age) {  //To easily create Person
+    this.id   = id;
+    this.name = name;
+    this.age  = age;
+  }
+
 }
